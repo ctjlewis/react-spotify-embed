399dbc239eb63b6666544a8296511d92e08006c5
C. Lewis <ctj.lewis@icloud.com>
2021-08-06 02:35:20 -0500

    patch: lint

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -8,7 +8,10 @@ import Spotify from '../src';
 describe('Spotify', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Spotify link="https://open.spotify.com/track/5ihDGnhQgMA0F0tk9fNLlA?si=14acaecc8a294b61" />, div);
+    ReactDOM.render(
+      <Spotify link="https://open.spotify.com/track/5ihDGnhQgMA0F0tk9fNLlA?si=14acaecc8a294b61" />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
